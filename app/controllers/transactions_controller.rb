1464c57fe5954d9a75e83b9a78392fac6bd8aff7
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-12-24 22:20:59 -0800

    Add filtering to transactions#get

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,8 @@
 class TransactionsController < ApplicationController
   def get
-    txns = Transaction.where(app_id: params[:id]).order(:id)
+    txns = Transaction.where(app_id: params.require(:app_id))
+      .where('id > ?', params.require(:last))
+      .order(:id)
     json = txns.map { |t| [t.id, t.key, t.value] }
     render json: json
   end
--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -6,7 +6,7 @@ describe TransactionsController do
   describe '#get' do
     context 'when the app does not exist' do
       it 'returns an empty array' do
-        get :get, id: app_id, start: 0
+        get :get, app_id: app_id, last: 0
         response.should be_ok
         json_response.should == []
       end
@@ -19,12 +19,18 @@ describe TransactionsController do
       end
 
       it 'returns the new records' do
-        get :get, id: app_id, start: 0
+        get :get, app_id: app_id, last: 0
         response.should be_ok
         json_response.should == [
           [1, "k1", "v1"],
           [2, "k2", "v2"],
         ]
+      end
+
+      it 'returns records starting at the requested point' do
+        get :get, app_id: app_id, last: 1
+        response.should be_ok
+        json_response.should == [[2, "k2", "v2"]]
       end
     end
   end
